aff3c860cb178a955dc588a4f0796ed2fce20638
Charlotte Mendy <charlotte.mendy.dev@gmail.com>
2021-08-17 18:31:51 +0200

    Add index.js : set target selectors

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+//& Target selectors
+//& ---------------------------------------------------------------------------
+const addTaskInput = document.getElementById('add-task-input');
+console.log(addTaskInput);
+const addTaskButton = document.getElementById('add-task-button');
+console.log(addTaskButton);
+const taskList = document.getElementById('task-list');
+console.log(taskList);
