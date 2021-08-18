c7f2afadb99c711a5381a324226a8a03d58bc785
Charlotte Mendy <charlotte.mendy.dev@gmail.com>
2021-08-18 10:58:11 +0200

    Create a task card

--- scripts/index.js
+++ scripts/index.js
@@ -16,6 +16,19 @@ function addTask() {
     addTaskButton.addEventListener('click', (e) => {
         console.log(e);
         e.preventDefault();
+
+        // Todo : create elements : div>li> button*2
+
+        // Create & append elements to be shown in the task list
+
+        // Creation of a task card : holds a task item (name and action buttons)
+        const taskCard = document.createElement('div');
+        // Add a class to this task card
+        taskCard.classList.add('task-card');
+        console.log(taskCard);
+        taskCard.innerHTML = 'Task card added';
+        // Append the task card to the task list
+        taskList.appendChild(taskCard);
     })
 };
 addTask();
