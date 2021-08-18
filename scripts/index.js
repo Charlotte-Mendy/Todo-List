a11d389c219b44125d5cc7429aa9e02621dfb998
Charlotte Mendy <charlotte.mendy.dev@gmail.com>
2021-08-18 10:45:58 +0200

    Create an add task function : first part handle the click event

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,21 @@
 //& Target selectors
 //& ---------------------------------------------------------------------------
+
 const addTaskInput = document.getElementById('add-task-input');
 console.log(addTaskInput);
 const addTaskButton = document.getElementById('add-task-button');
 console.log(addTaskButton);
 const taskList = document.getElementById('task-list');
 console.log(taskList);
+
+//& Adding a new task
+//& ---------------------------------------------------------------------------
+
+function addTask() {
+    // Event listener
+    addTaskButton.addEventListener('click', (e) => {
+        console.log(e);
+        e.preventDefault();
+    })
+};
+addTask();
